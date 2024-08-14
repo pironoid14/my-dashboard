02d185bf1e70e6e5e4cde1f66c9dcbbe48370b14
pironoid14 <56125168+pironoid14@users.noreply.github.com>
2024-08-14 12:38:38 +0100

    added dashboard ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { Route, Routes } from "react-router-dom"
 import { Login } from "./pages/login"
 import { Register } from "./pages/register"
+import { Home } from "./pages/home"
+
 
 
 function App() {
@@ -9,6 +11,7 @@ function App() {
   return (
     <>
       <Routes>
+        <Route path="/home" element = {<Home/>}/>
       <Route path="/login" element = {<Login/>}/>
           <Route path="/register" element = {<Register/>}/>
           <Route/>
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,19 +1,39 @@
+import { Link } from "react-router-dom";
 
 
 
 
-const Home = () => {
+
+export const Home = () => {
   return (
     <div>
-      <nav className='flex justify-center items-center h-16 bg-blue-500 text-white font-bold text-xl'>
+      <nav className='bg-amber-300 grid grid-cols-3'>
+        <div className="justify-start ">
+      <h1 className="text-rose-900 font-bold  p-4 ">Journal</h1>
+      </div>
+      <div className="justify-center">
         <ul className='flex space-x-4 '>
           <li>Home</li>
-          <li className='bg-red-500'></li>
-          <li className='bg-red-500 text-white'></li>
+          <li >about</li>
+          <li >feature</li>
+          <li>Docs</li>
+          <li>Pricing</li>
         </ul>
+        </div>
+        <div className="justify-end">
+        <Link to ="/login"> Login</Link>
+        <Link to ="/register"> Register</Link>
+        </div>
+
       </nav>
       <section>
-
+        <div>
+          <h2 className="text-2xl text-center font-bold mt-4">Write your daily Journal and <br/> read them in an exciting way</h2>
+          <p className="text-xl mt-4 text-center">Lorem ipsum, dolor sit amet consectetur <br/> adipisicing elit. Sed quia eius fuga id maxime consequuntur mollitia corrupti eaque voluptates debitis quas illo <br/> ipsum sunt laborum, numquam maiores facere a est.</p>
+        </div>
+        <div className="flex justify-center ">
+         <button className="p-4  mt-4 rounded-full bg-rose-900">Get Started</button>
+        </div>
       </section>
     </div>
   )
