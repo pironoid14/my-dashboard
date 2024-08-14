e44b1ca769a094f27b67529b6cf88802664ad3ab
pironoid14 <56125168+pironoid14@users.noreply.github.com>
2024-08-14 20:40:08 +0100

    linked login to dashboard

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -67,11 +67,11 @@ const handleSubmit = (e: React.FormEvent<HTMLButtonElement>) => {
         {errors.password && <span className="text-red-500 text-sm">{errors.password}</span>}
       </div>
   
-      <button type="submit" className="w-full bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600" 
+      
+      <Link to ="/home"><button type="submit" className="w-full bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600" 
       onSubmit={handleSubmit}>
         Login
-      </button>
-      <Link to ="/home"></Link>
+      </button></Link>
     </form>
   </div>
   
