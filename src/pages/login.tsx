eb78484b1c503c13618cca39e282aecaea62f03c
pironoid14 <56125168+pironoid14@users.noreply.github.com>
2024-08-26 19:08:20 +0100

    implemented useform in login

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,66 +1,57 @@
-
-import {useState} from 'react'
+import { SubmitHandler, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
 import { Link } from 'react-router-dom';
 
-
+const schema = z.object({ 
+  email: z.string().email(),
+  password: z.string().min(8)
+})
+type inputField = z.infer<typeof schema>;
 export const Login = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [errors, setErrors] = useState<{ [key: string]: string }>({});
+  const {register, handleSubmit, formState: {errors}} = useForm<inputField>({
+    resolver: zodResolver(schema)
+});
+const onSubmit: SubmitHandler<inputField> = data => console.log(data);
   
-const validate = () => {
-  const perrors: { [key: string]: string } = {};
-  if (!email) perrors.email = "Email is required";
-  else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(email)) perrors.email = "Invalid email address";
-  if (!password) perrors.password = "Password is required";
-  else if (password.length < 8) perrors.password = "Password must be at least 8 characters";
-  return perrors;
-}
-
-const handleLogin = (e: React.FormEvent) => {
-  e.preventDefault();
-  const ring = validate();
-  
-  if (Object.keys(errors).length > 0) {
-    setErrors(ring);; 
-  } else {
-    setErrors({});
-    console.log({email, password});
-  }
-  
-}
   return (
     <div className="bg-white h-screen flex justify-center items-center ">
-    <form  className="w-full max-w-xs p-6 bg-white rounded-md shadow-lg border-2 border-black ">
+    <form  className="w-full max-w-xs p-6 bg-white rounded-md shadow-lg border-2 border-black" onSubmit={handleSubmit(onSubmit)}>
       <h1 className="text-2xl font-bold mb-6 text-center">Login</h1>
   
       <div className="mb-4">
         <label className="block text-gray-700 mb-2" htmlFor="email">Email:</label>
         <input
+        {...register("email", { required: true })}
           type="email"
           id="email"
           className="w-full border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
+         
         />
-        {errors.email && <span className="text-red-500 text-sm">{errors.email}</span>}
-      </div>
+       {errors.email && (
+              <span className="text-red-500 text-sm">{errors.email.message}</span>
+       )}
+              </div>
   
       <div className="mb-6">
         <label className="block text-gray-700 mb-2" htmlFor="password">Password:</label>
         <input
+        {...register("password", { required: true })}
           type="password"
           id="password"
           className="w-full border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
+          
         />
-        {errors.password && <span className="text-red-500 text-sm">{errors.password}</span>}
-      </div>
+       {errors.password && (
+              <span className="text-red-500 text-sm">{errors.password.message}</span>
+      
+       )}
+      
+          </div>
   
       
       <Link to ="/" ><button type="submit" className="w-full bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-yellow-600" 
-      onSubmit={handleLogin}>
+      >
         Login
       </button></Link>
       <Link to="/ForgotPassword"><button type='submit' className="w-full bg-rose-500 text-white px-4 py-2 rounded-md hover:bg-teal-600">Forgot Password </button></Link>
